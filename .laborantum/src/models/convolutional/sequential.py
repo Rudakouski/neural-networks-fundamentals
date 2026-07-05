d3f43929235c9ba26abf93c1edf87c18eb8585e8
Rudakouski <yanrudakovski@gmail.com>
2026-07-05 14:43:52 +0000

    Save current course progress

--- .laborantum/src/models/convolutional/sequential.py
+++ .laborantum/src/models/convolutional/sequential.py
@@ -17,11 +17,35 @@ class ResidualBottleneck(torch.nn.Module):
         self.bypass = torch.nn.Identity()
         self.residual = residual
 
-        ## YOUR CODE HERE
+        hidden_channels = max(1, in_channels // compression)
+
+        self.prenorm = prenormalization(in_channels)
+        self.postnorm = postnormalization(out_channels)
+        self.activation = activation()
+
+        self.block = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels, hidden_channels, kernel_size=1),
+            self.activation,
+            torch.nn.Conv2d(hidden_channels, out_channels, kernel_size=1),
+        )
+
+        if residual and in_channels != out_channels:
+            self.bypass = torch.nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        else:
+            self.bypass = torch.nn.Identity()
 
     def forward(self, signal):
         ## YOUR CODE HERE
+        x = self.prenorm(signal)
 
+        out = self.block(x)
+
+        if self.residual:
+            out = out + self.bypass(x)
+
+        out = self.postnorm(out)
+
+        signal = out
         return signal
 
 
@@ -35,14 +59,26 @@ class FullyConvolutionalNN(torch.nn.Module):
             n_blocks=[1, 1, 1, 1]):
         ...
         ## YOUR CODE HERE
-        # Define network modules in the constructor
+        super().__init__()
+        ## YOUR CODE HERE
+
+        dims = [28 * 28] + mid_channels + [out_channels]
+
+        layers = []
+        for i in range(len(dims) - 1):
+            layers.append(torch.nn.Linear(dims[i], dims[i + 1]))
+            if i < len(dims) - 2:
+                layers.append(torch.nn.ReLU())
+
+        self.model = torch.nn.Sequential(*layers)
 
 
     def __forward_kernel(self, signal):
         ## YOUR CODE HERE
         # Pass the signal through the modules in forward
 
-
+        signal = signal.view(signal.size(0), -1)  # [B, 784]
+        signal = self.model(signal)
         return signal
 
     def forward(self, batch):
@@ -62,7 +98,7 @@ class FullyConvolutionalNN(torch.nn.Module):
         # Take network's output from the batch
         signal = batch['signals']['output']
 
-        ## YOUR CODE HERE
+        signal = torch.argmax(signal, dim=1)
 
         # Put the processed result into the batch
         batch['postprocessed'] = {'class': signal}
